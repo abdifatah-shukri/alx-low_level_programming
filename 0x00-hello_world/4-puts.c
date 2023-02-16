9fe91efdf2a91099370fde2a0db778ac688f325a
abdifatah-shukri <abdifatahshukri15@gmail.com>
2023-02-16 14:29:12 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Reaturn: Always 0 (succes)
+ */
+
+int main (void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+                    
+                return (0);
+}	
